146c7d6403dcd779de15fe9f792cae4ac000079e
emma-martin <emmamartinalvarez@gmail.com>
2018-12-24 18:06:26 +0100

    Accept mp4 format

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,28 +1,67 @@
 'use strict';
 
-console.log('>> Ready :)');
-
-const complimentEl = document.querySelector('.compliment');
-const puppyImg = document.querySelector('.img');
-
+const card = document.querySelector('.card');
 const btnCompliment = document.querySelector('.btn');
-// const complimentJson = require('./assets/json/compliment.json');
-// console.log(complimentJson);
 const urlCompliment = './assets/json/compliment.json';
 const urlPuppies = 'https://random.dog/woof.json';
 
+//DOM elements (h2 compliment img puppy)
+function createElement(tag, newClass){
+  const newEl = document.createElement(tag);
+  newEl.classList.add(newClass);
+  return newEl;
+}
+
+function createText(text){
+  const newText = document.createTextNode(text);
+  return newText;
+}
+
+function uniteElementText(tag, newClass, text){
+  const newTitle = createElement(tag, newClass);
+  const newTextEl = createText(text);
+  newTitle.appendChild(newTextEl);
+  return newTitle;
+}
+
+function createImg(src, alt){
+  const image = createElement('img', 'img');
+  image.setAttribute('src', src);
+  image.setAttribute('alt', alt);
+  return image;
+}
+
+function createVideo(src){
+  const newVid = createElement('video', 'video');
+  newVid.setAttribute('autoplay','');
+  const vidSource = createElement('source');
+  vidSource.setAttribute('src', src);
+  vidSource.setAttribute('type', 'video/mp4');
+  newVid.appendChild(vidSource);
+  return newVid;
+}
+
+card.innerHTML = `<h1 class="title--intro">Cosas bonitas para Ilse</h1>`;
 function getCompliment(){
+  card.innerHTML = '';
   fetch(urlCompliment)
     .then(response => response.json())
     .then(function(data){
       const compliment = data.feed.entry[getRandom()].gsx$compliments.$t;
-      complimentEl.innerHTML = `${compliment}`;
+      const newCompliment = uniteElementText('h3', 'title', compliment);
+      card.appendChild(newCompliment);
     })
     .then(fetch(urlPuppies)
       .then(response => response.json())
       .then(function(data){
         const puppy = data.url;
-        puppyImg.src = puppy;
+        if(puppy.includes('.mp4')){
+          const newVidPup = createVideo(puppy);
+          card.appendChild(newVidPup);
+        }else {
+          const newPup = createImg(puppy, 'puppy for Ilse');
+          card.appendChild(newPup);
+        }
       })
     );
 }
